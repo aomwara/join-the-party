653f3a5f4d153f4341aaae0a53652d133592ca98
aom wara <waranat.cpe@gmail.com>
2022-05-03 19:59:54 +0700

    feat: authentication API

---
+++ backend/src/routes/user.route.ts
@@ -0,0 +1,40 @@
+import express from "express";
+import {
+  loginHandler,
+  logoutHandler,
+  refreshAccessTokenHandler,
+  registerHandler,
+} from "../controllers/auth.controller";
+import {
+  getAllUsersHandler,
+  getMeHandler,
+} from "../controllers/user.controller";
+import { deserializeUser } from "../middleware/deserializeUser";
+import { requireUser } from "../middleware/requireUser";
+import { restrictTo } from "../middleware/restrictTo";
+import { validate } from "../middleware/validate";
+import { createUserSchema, loginUserSchema } from "../schema/user.schema";
+
+const router = express.Router();
+
+// Register user route
+router.post("/register", validate(createUserSchema), registerHandler);
+
+// Login user route
+router.post("/login", validate(loginUserSchema), loginHandler);
+
+// Refresh access toke route
+router.get("/refresh", refreshAccessTokenHandler);
+
+router.use(deserializeUser, requireUser);
+
+// Logout User
+router.get("/logout", logoutHandler);
+
+// Admin Get Users route
+router.get("/", restrictTo("admin"), getAllUsersHandler);
+
+// Get my info route
+router.get("/me", getMeHandler);
+
+export default router;
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import Navbar from "../components/Utils/Navbar";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ChakraProvider>
+      <Navbar />
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+};
 
-export default MyApp
+export default App;
